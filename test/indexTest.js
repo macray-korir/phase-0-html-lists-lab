d728e447593d2879e55357e4fe711e5c9e55563d
macray <macraykorir254@gmail.com>
2023-06-07 14:18:56 +0300

    Completed Assignment

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,41 +1,37 @@
-require ( './helpers.js' );
+require('./helpers.js');
 
-const chai = require("chai");
-chai.use(require("chai-dom"));
+const chai = require('chai');
+chai.use(require('chai-dom'));
 const { expect } = chai;
 
-describe("the <ul> tag", () => {
-  it("exists in the document", () => {
-    // find the first <ul> in the HTML file
-    const ul = document.querySelector("ul");
-    const hint = "The document should have a <ul> element";
+describe('the <ul> tag', () => {
+  it('exists in the document', () => {
+    const ul = document.querySelector('ul');
+    const hint = 'The document should have a <ul> element';
 
     expect(ul, hint).to.exist;
   });
 
-  it("has three child <li> tags with the correct content", () => {
-    // find all direct child <li> elements in the <ul>
-    const ul = document.querySelector("ul");
+  it('has three child <li> tags with the correct content', () => {
+    const ul = document.querySelector('ul');
 
-    const hint = "The <ul> should have three <li> elements nested inside";
+    const hint = 'The <ul> should have three <li> elements nested inside';
     expect(ul.children, hint).to.have.lengthOf(3);
 
-    // check the values of each <li> element
     const [firstLi, secondLi, thirdLi] = ul.children;
 
-    expect(firstLi).to.have.tagName("li");
-    expect(firstLi).to.contain.text("2 slices of bread");
+    expect(firstLi).to.have.tagName('li');
+    expect(firstLi).to.contain.text('2 slices of bread');
 
-    expect(secondLi).to.have.tagName("li");
-    expect(secondLi).to.contain.text("4 slices of cheese");
+    expect(secondLi).to.have.tagName('li');
+    expect(secondLi).to.contain.text('4 slices of cheese');
 
-    expect(thirdLi).to.have.tagName("li");
-    expect(thirdLi).to.contain.text("1 tbsp of butter");
+    expect(thirdLi).to.have.tagName('li');
+    expect(thirdLi).to.contain.text('1 tbsp of butter');
   });
 
-  it("contains a nested <ul> tag within a <li>", () => {
-    // find a <ul> nested inside a <li> nested inside a <ul>
-    const ul = document.querySelector("ul");
+  it('contains a nested <ul> tag within a <li>', () => {
+    const ul = document.querySelector('ul');
     const hint = `
       The <ul> must contain another <ul> nested *inside* a <li>:
       <ul>
@@ -46,11 +42,12 @@ describe("the <ul> tag", () => {
       </ul>
     `;
 
-    expect(ul, hint).to.have.descendant("li > ul");
+    const secondLi = ul.children[1];
+    expect(secondLi, hint).to.have.descendant('ul');
   });
 
-  it("contains three <li> nested within the nested <ul> with the correct content", () => {
-    const ul = document.querySelector("ul");
+  it('contains three <li> nested within the nested <ul> with the correct content', () => {
+    const ul = document.querySelector('ul');
     const hint = `
       The *nested* <ul> must contain three nested <li>:
       <ul>
@@ -65,31 +62,51 @@ describe("the <ul> tag", () => {
       </ul>
     `;
 
-    expect(ul, hint).to.have.descendants("li > ul > li").and.have.length(3);
+    const nestedUl = ul.querySelector('li ul');
+    expect(nestedUl, hint).to.have.descendants('li').and.have.length(3);
+
+    const [cheddarLi, mozzarellaLi, pepperJackLi] = nestedUl.children;
+
+    expect(cheddarLi).to.have.tagName('li');
+    expect(cheddarLi).to.contain.text('cheddar');
+
+    expect(mozzarellaLi).to.have.tagName('li');
+    expect(mozzarellaLi).to.contain.text('mozzarella');
+
+    expect(pepperJackLi).to.have.tagName('li');
+    expect(pepperJackLi).to.contain.text('pepper jack');
   });
 });
-describe("the <ol> tag", () => {
-  it("exists in the document", () => {
-    // find the first <ol> in the HTML file
-    const ol = document.querySelector("ol");
-    const hint = "The document should have a <ol> element";
+
+describe('the <ol> tag', () => {
+  it('exists in the document', () => {
+    const ol = document.querySelector('ol');
+    const hint = 'The document should have an <ol> element';
 
     expect(ol, hint).to.exist;
   });
 
-  it("has five child <li> tags with the correct content", () => {
-    // find all direct child <li> elements in the <ul>
-    const ol = document.querySelector("ol");
-    const hint1 = "The <ol> should have five <li> elements";
-    expect(ol, hint1).to.have.descendants("li").and.have.length(5);
+  it('has five child <li> tags with the correct content', () => {
+    const ol = document.querySelector('ol');
+    const hint = 'The <ol> should have five <li> elements';
 
-    // Check the content of each <li>
-    const [first, second, third, fourth, fifth] = ol.querySelectorAll("li");
+    expect(ol.children, hint).to.have.lengthOf(5);
 
-    expect(first).to.contain.text("Spread butter on bread and frying pan");
-    expect(second).contain.text("Place bread in frying pan and fry");
-    expect(third).contain.text("Add cheese on top of bread");
-    expect(fourth).contain.text("Cover with second slice of bread");
-    expect(fifth).contain.text("Turn over and fry for 2 minutes");
+    const [firstLi, secondLi, thirdLi, fourthLi, fifthLi] = ol.children;
+
+    expect(firstLi).to.have.tagName('li');
+    expect(firstLi).to.contain.text('Spread butter on bread and frying pan');
+
+    expect(secondLi).to.have.tagName('li');
+    expect(secondLi).to.contain.text('Place bread in frying pan and fry');
+
+    expect(thirdLi).to.have.tagName('li');
+    expect(thirdLi).to.contain.text('Add cheese on top of bread');
+
+    expect(fourthLi).to.have.tagName('li');
+    expect(fourthLi).to.contain.text('Cover with second slice of bread');
+
+    expect(fifthLi).to.have.tagName('li');
+    expect(fifthLi).to.contain.text('Turn over and fry for 2 minutes');
   });
 });
